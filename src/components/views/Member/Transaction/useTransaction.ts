561492ddf09a0eb60bc294d7a4c0427cbb9377ff
SamudraWijayas <swijayasamdoria@gmail.com>
2025-10-06 18:10:52 +0700

    transaction

--- src/components/views/Member/Transaction/useTransaction.ts
+++ src/components/views/Member/Transaction/useTransaction.ts
@@ -1,27 +1,31 @@
 import useChangeUrl from "@/hooks/useChangeUrls";
 import orderServices from "@/services/order.service";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 const useTransaction = () => {
+  const router = useRouter();
   const { currentLimit, currentPage } = useChangeUrl();
 
   const getMemberTransactions = async () => {
     const params = `limit=${currentLimit}&page=${currentPage}`;
     const res = await orderServices.getMemberOrder(params);
-    const { data } = res;
-    return data;
+    return res.data;
   };
 
-  const { data: dataTransactions, isLoading: isLoadingTransactions } = useQuery(
-    {
-      queryKey: ["Banners"],
-      queryFn: getMemberTransactions,
-    },
-  );
+  const {
+    data: dataTransactions,
+    isLoading: isLoadingTransactions,
+    isFetching,
+  } = useQuery({
+    queryKey: ["transactions", currentPage, currentLimit], // ✅ dinamis & unik per page
+    queryFn: getMemberTransactions,
+    enabled: router.isReady && !!currentPage && !!currentLimit, // ✅ fetch hanya jika param siap
+  });
 
   return {
     dataTransactions,
-    isLoadingTransactions,
+    isLoadingTransactions: isLoadingTransactions || isFetching,
   };
 };
 
